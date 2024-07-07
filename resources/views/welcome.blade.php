474ca0247fb8bd547fc9fa3fb86820641f7c4b1f
saddadnabbil <saddadnabbil>
2024-07-07 09:49:44 +0700

    fix: column panel switch (welcome)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
             </div>
 
             <div class="mt-16">
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
                     @php 
                         $user = auth()->user();
                     @endphp
